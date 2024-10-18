37548102c37e2abb8228802d4159b6c3d01d40c0
daw22 <dawitxxd@gmail.com>
2024-10-18 15:09:12 +0300

    ai api key fix

--- chat-server/utils/ai.js
+++ chat-server/utils/ai.js
@@ -4,7 +4,7 @@ import  {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = process.env.AI_API_KEY;
+const apiKey = process.env.AI_API;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const safetySettings = [
