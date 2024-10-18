b1082a93a26372f2d1156822bb0c54d3c96ec53c
daw22 <dawitxxd@gmail.com>
2024-10-18 13:21:58 +0300

    url change to deployed backend

--- chat-client/src/utils/axinstance.js
+++ chat-client/src/utils/axinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your actual API base URL
+  baseURL: 'https://talkverse-api.onrender.com', // Replace with your actual API base URL
 });
 
 axiosInstance.interceptors.request.use(
